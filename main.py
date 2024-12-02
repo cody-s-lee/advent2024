92bc06d6e6b603d1cd7f77355020479ac55e5ceb
Cody Lee <cody.s.lee@gmail.com>
2024-12-02 08:47:23 -0500

    Day 2 part 2

--- main.py
+++ main.py
@@ -91,7 +91,61 @@ def day02(reader):
             print('safe')
             result_a += 1
 
-    return result_a, result_a
+    result_b = 0
+    for i, line in enumerate(reader):
+        if len(line) == 0:
+            continue
+
+        print(f'{i}: {line}: ', end='')
+
+        levels = [int(l) for l in line.split(" ")]
+
+        is_safe = False
+
+        direction = 0
+        for (x, y) in pairwise(levels):
+            if abs(x - y) <1 or abs(x-y) > 3:
+                break # unsafe
+
+            if direction == 0:
+                direction = 1 if x < y else -1
+
+            if x < y and direction == -1:
+                break #unsafe
+            if x > y and direction == 1:
+                break #unsafe
+        else:
+            # safe
+            is_safe = True
+
+        if not is_safe:
+            for j in range(len(levels)):
+                sub_levels = levels[:j] + levels[j+1:]
+                direction = 0
+                for (x, y) in pairwise(sub_levels):
+                    if abs(x - y) < 1 or abs(x - y) > 3:
+                        break  # unsafe
+
+                    if direction == 0:
+                        direction = 1 if x < y else -1
+
+                    if x < y and direction == -1:
+                        break  # unsafe
+                    if x > y and direction == 1:
+                        break  # unsafe
+                else:
+                    # safe
+                    is_safe = True
+                    break
+
+        if is_safe:
+            print('safe')
+            result_b += 1
+        else:
+            print('unsafe')
+
+
+    return result_a, result_b
 
 
 def do(reader, processor):
