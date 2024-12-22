089f73abcbfe5d4361a4612d14dd9e8185f9f599
Cody Lee <cody.s.lee@gmail.com>
2024-12-22 19:11:04 -0400

    foo

--- day22.py
+++ day22.py
@@ -1,43 +1,67 @@
 import itertools
-from functools import cache, lru_cache
+from functools import cache, lru_cache, partial
 from multiprocessing import Manager, Pool
 
 from utils import with_lines
 
+global num_lines
+global lock
+global doing
+global done
 
-def sx(i):
+
+def sx(args):
+    i, x = args
     lock.acquire()
-    lock.value += 1
-    print(f'\rIn progress: {doing.value}, completed: {done.value}/{len(secrets)}        ', end='')
+    doing.value += 1
+    print(f'\rIn progress: {doing.value}, completed: {done.value}/{num_lines}        ', end='')
     lock.release()
 
-    secquences[i] = secquence_2k(secrets[i])
+    result = (i, sequence_2k(x))
 
     lock.acquire()
     doing.value -= 1
     done.value += 1
-    print(f'\rIn progress: {doing.value}, completed: {done.value}/{len(secrets)}        ', end='')
+    print(f'\rIn progress: {doing.value}, completed: {done.value}/{num_lines}        ', end='')
     lock.release()
 
+    return result
 
-def ix(i):
-    secq = secquences[i]
-    _insets = insets_x(secq)
+
+def ix(args: tuple[int, list[int]], num_sequences: int) -> list[tuple[tuple[int, tuple[int, ...]], int]]:
+    i, seq = args
+    
+    _insets = insets(seq)  # sequence -> inset tuple, value
+    results = []
     for inset, value in _insets:
-        inset_maps[(i, inset)] = value
+        if (i, inset) not in results:
+            results.append(((i, inset), value))
+
+    lock.acquire()
+    done.value += 1
+    print(f'\rProcessed {done.value}/{num_sequences} entries', end='')
+    lock.release()
+
+    return results  # list[ ( (i,inset), value ) ]
 
 
-def insets_x(secq: list[int]) -> list[tuple[tuple[int, ...], int]]:  # start pos, inset tuple, end value
+def insets(seq: list[int]) -> list[tuple[tuple[int, ...], int]]:  # start pos, inset tuple, end value
     results = []
 
-    deltas = [(j % 10) - (i % 10) for i, j in itertools.pairwise(secq)]
+    deltas = [(j % 10) - (i % 10) for i, j in itertools.pairwise(seq)]
+    insets_seen = set()
     for start in range(len(deltas) - 4):
-        results.append((tuple(deltas[start:start + 4]), secq[start + 4] % 10))
+        inset = tuple(deltas[start:start + 4])
+        if inset in insets_seen:
+            continue
+
+        results.append((inset, seq[start + 4] % 10))
+        insets_seen.add(inset)
 
     return results
 
 
-def init_pool_processes(the_lock, the_doing, the_done, the_secquences, the_secrets, the_inset_maps, the_all_insets):
+def init_pool_processes(the_lock, the_doing, the_done, the_num_lines):
     global lock
     lock = the_lock
 
@@ -47,65 +71,56 @@ def init_pool_processes(the_lock, the_doing, the_done, the_secquences, the_secre
     global done
     done = the_done
 
-    global secquences
-    secquences = the_secquences
-
-    global secrets
-    secrets = the_secrets
-
-    global inset_maps
-    inset_maps = the_inset_maps
-
-    global all_insets
-    all_insets = the_all_insets
+    global num_lines
+    num_lines = the_num_lines
 
 
 @with_lines
 def day22(lines):
+    init_secrets = [(i, int(l)) for i, l in enumerate(lines)]
     num_lines = len(lines)
 
     manager = Manager()
-    _init_secrets = manager.dict({i: int(l) for i, l in enumerate(lines)})
-    _lock = manager.Lock(),
-    _doing = manager.Value('i', 0),
-    _done = manager.Value('i', 0),
-    _secquences = manager.dict(),
-    _secrets = _init_secrets,
-    _inset_maps = manager.dict(),
-    _all_insets = manager.list(),
-    pool = Pool(processes=64, initializer=init_pool_processes,
-                initargs=(_lock, _doing, _done, _secquences, _secrets, _inset_maps, _all_insets,))
 
-    print(f'Processing secquences:')
-    pool.imap_unordered(sx, range(num_lines))
-    print(f'\rIn progress: {_doing.value}, completed: {_done.value}/{len(secrets)}        ')
+    lock = manager.Lock()
+    doing = manager.Value('i', 0)
+    done = manager.Value('i', 0)
 
-    result_a = sum(s[2000] for s in _secquences.values())
+    pool = Pool(processes=64, initializer=init_pool_processes, initargs=(lock, doing, done, num_lines,))
 
-    print(f'Processing insets:')
-    pool.imap_unordered(ix, range(num_lines))
+    print(f'Processing sequences:')
+    sequences = {k: v for k, v in pool.imap_unordered(sx, init_secrets)}
+    print(f'\rIn progress: {doing.value}, completed: {done.value}/{num_lines}        ')
+
+    result_a = sum(s[2000] for s in sequences.values())
+
+    print(f'Processing insets...')
+    done.value = 0
+    inset_map_entry_lists = pool.imap_unordered(partial(ix, num_sequences=len(sequences)), sequences.items())
+    inset_map = dict()
+    for entry_list in inset_map_entry_lists:
+        for key, value in entry_list:
+            inset_map[key] = value
+    print(f'\rProcessed {len(inset_map)}/{len(sequences)} entries')
 
     print(f'Processing all_insets:')
-    _all_insets.extend(ins for ins in {inset for _, inset in _inset_maps.keys()})
+    all_insets = {ins for _, ins in inset_map.keys()}
 
     print(f'Processing profits:')
-    profits = [pft for pft in pool.imap_unordered(pfx, all_insets)]
-    max_profit = max(profits)
+    inset_profit_map = dict()
+    for inset in all_insets:
+        # print(f'Processing inset: {inset}', end='')
+        inset_profit = 0
+        for i in range(num_lines):
+            inset_profit += inset_map.get((i, inset), 0)
+        inset_profit_map[inset] = inset_profit
+        # print(f'\rProcessing inset: {inset} -> {inset_profit}')
+
+    result_b = max(inset_profit_map.values())
 
     # 2445
 
-    return result_a, max_profit
-
-
-def pfx(inset):
-    pft = 0
-    for i in range(len(secrets)):
-        pft + inset_maps.get((i, inset), 0)
-    return pft
-
-
-def profit_for_secquences(ins, secquences) -> int:
-    return sum(profit(secq, ins) for secq in secquences)
+    return result_a, result_b
 
 
 @cache
@@ -116,35 +131,30 @@ def secret(x, n=1):
     return x
 
 
-def secquence(x: int, n: int) -> list[int]:
+def sequence(x: int, n: int) -> list[int]:
     return [secret(x, i) for i in range(0, n + 1)]
 
 
-def secquence_2k(x: int) -> list[int]:
-    # print(f'Calculating secquence for {x}')
-    result = secquence(x, 2000)
-    # print(f'Finished calculating secquence for {x}')
+def sequence_2k(x: int) -> list[int]:
+    # print(f'Calculating sequence for {x}')
+    result = sequence(x, 2000)
+    # print(f'Finished calculating sequence for {x}')
     return result
 
 
-def profit(secq: list[int] | tuple[int], inseq: list[int] | tuple[int, int, int, int]) -> int:
-    return _profit(tuple(secq), tuple(inseq))
+def profit(seq: list[int] | tuple[int], inseq: list | tuple) -> int:
+    return _profit(tuple(seq), tuple(inseq))
 
 
 @lru_cache
-def _profit(secq: tuple[int], inseq: tuple[int, int, int, int]) -> int:
-    deltas = [(j % 10) - (i % 10) for i, j in itertools.pairwise(secq)]
+def _profit(seq: tuple[int], inseq: tuple) -> int:
+    deltas = [(j % 10) - (i % 10) for i, j in itertools.pairwise(seq)]
 
     for i in range(0, len(deltas) - 4):
         if tuple(deltas[i:i + 4]) == inseq:
-            return secq[i + 4] % 10
+            return seq[i + 4] % 10
 
     return 0
-
-
-def insets(secq: list[int]) -> set[tuple[int, int, int, int]]:
-    deltas = [(j % 10) - (i % 10) for i, j in itertools.pairwise(secq)]
-    return set((deltas[i], deltas[i + 1], deltas[i + 2], deltas[i + 3]) for i in range(len(deltas) - 4))
 
 
 @cache
@@ -164,17 +174,17 @@ def test_secret():
     assert secrets == [15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254]
 
 
-def test_secquence():
+def test_sequence():
     x = 123
-    secrets = secquence(x, 10)
+    secrets = sequence(x, 10)
     assert secrets == [123, 15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254]
 
 
 def test_profit():
     x = 2024
-    secq = secquence(x, 2000)
+    seq = sequence(x, 2000)
 
-    assert profit(secq, [-2, 1, -1, 3]) == 9
+    assert profit(seq, [-2, 1, -1, 3]) == 9
 
     pass
 
@@ -183,9 +193,9 @@ def test_part2():
     initial_secrets = [1, 2, 3, 2024]
 
     pool = Pool(20)
-    secquences = pool.map(secquence_2k, initial_secrets)
+    sequences = pool.map(sequence_2k, initial_secrets)
 
-    all_ins = {ins for secq in secquences for ins in insets(secq)}
+    all_ins = {ins for seq in sequences for ins in insets(seq)}
     profits: dict[tuple, int] = dict()
     print(f'All ins: {len(all_ins)}')
 
@@ -196,7 +206,7 @@ def test_part2():
             print(f'Progress: {n}/{len(all_ins)}, max_profit: {max_profit}')
         n += 1
 
-        profits[ins] = sum(profit(secq, ins) for secq in secquences)
+        profits[ins] = sum(profit(seq, ins) for seq in sequences)
         max_profit = max(max_profit, profits[ins])
 
     assert max_profit == 23
